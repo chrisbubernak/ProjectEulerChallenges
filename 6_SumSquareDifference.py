858dc61e08d5f4b509c47e73cd8db46ca3f7fc96
Chris Bubernak <chris.bubernak@gmail.com>
2014-08-01 13:30:33 -0700

    converted #6 over from javascript and committing it

---
+++ 6_SumSquareDifference.py
@@ -0,0 +1,15 @@
+#Problem 6
+#Find the difference between the sum of the squares of the 
+#first one hundred natural numbers and the square of the sum.
+
+def solve(n):
+  sumOfSquares = 0;
+  squareOfSums = 0;
+  for i in range(0, n+1):
+    sumOfSquares += (i*i);
+    squareOfSums += i;
+  squareOfSums = (squareOfSums*squareOfSums);
+  return squareOfSums - sumOfSquares;
+
+print solve(100);
+
