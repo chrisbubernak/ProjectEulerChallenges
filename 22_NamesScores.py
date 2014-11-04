2e16a78f618bbc959bd2c62f5df316551498590e
Chris Bubernak <chris.bubernak@gmail.com>
2014-11-04 13:07:53 -0800

    solved 22!

---
+++ 22_NamesScores.py
@@ -0,0 +1,32 @@
+#Using names.txt (right click and 'Save Link/Target As...'), a 46K text file
+#containing over five-thousand first names, begin by sorting it into alphabetical
+#order. Then working out the alphabetical value for each name, multiply this value
+#by its alphabetical position in the list to obtain a name score.
+
+#For example, when the list is sorted into alphabetical order, COLIN, which is
+#worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would
+#obtain a score of 938 * 53 = 49714.
+
+#What is the total of all the name scores in the file?
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+def score(name):
+    ret = 0;
+    chars = list(name);
+    for i in range(len(chars)):
+        ret = ret + (alphabet.index(chars[i]) + 1);
+    return ret;
+
+def solve(names):
+    nameList = names.split();
+    nameList.sort();
+    ret = 0;
+    for i in range(len(nameList)):
+        ret = ret + (i + 1) * score(nameList[i]);
+    return ret;
+
+with open ("names.txt", "r") as myFile:
+    names = myFile.read().replace(',', ' ').replace('"', '');
+
+    print solve(names);
