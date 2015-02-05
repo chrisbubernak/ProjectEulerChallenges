3e6bd99183c8a71f590acf9ddff29c466241c137
Chris Bubernak <chris.bubernak@gmail.com>
2015-02-05 15:53:28 -0800

    improving answer to 39!

--- 39_IntegerRightTriangles.py
+++ 39_IntegerRightTriangles.py
@@ -5,31 +5,22 @@
 
 # For which value of p <= 1000, is the number of solutions maximised?
 
-def getSolutions2(p):
+def getSolutions(p):
     ret = 0;
-    for i in range(2, p):
-        for j in range(p - (2 * i), 1, -1):
-            k = p - i - j;
-            if j > k:
-                break;
-            k2 = k * k;
-            i2 = i * i;
-            j2 = j * j;
-            if (k2 > (i2 + j2)):
-                break;
-            if k2 == (i2 + j2):
-                ret = ret + 1;
-                break;
+    for i in range(1, p):
+        if (p * p - 2 * p * i) % (2 * p - 2 * i) == 0:
+            ret = ret + 1;
     return ret;
 
 def solve(n):
     best = 0;
     ret = 0;
     for i in range(1, n + 1):
-        s = getSolutions2(i);
+        s = getSolutions(i);
         if s > best:
             best = s;
             ret = i;
     return ret;
 
 print solve(1000);
+
