a303fead54022195fa693a1d268ab13044198179
Chris Bubernak <chris.bubernak@gmail.com>
2021-02-13 20:13:26 -0800

    number 53!

---
+++ 53_CombinatoricSelections.py
@@ -0,0 +1,39 @@
+# There are exactly ten ways of selecting three from five, 12345:
+
+# 123, 124, 125, 134, 135, 145, 234, 235, 245, and 345
+
+# In combinatorics, we use the notation, 
+
+# (5 / 3) = 10
+
+# In general, (n / r) = n! / r!(n - r)!, where r <= n, n!= n x (n - 1) x ... x 3 x 2 x 1 and 0! = 1
+ 
+# It is not until n = 23, that a value exceeds one-million: (23 / 10) 1144066
+
+# How many, not necessarily distinct, values of (n / r) for 1 <= n <= 100, are greater than one-million?
+
+import math
+
+factorialCache = dict()
+
+# wrapper around the built in factorial method with a cache, this makes things way faster!
+def factorial(n):
+  if n in factorialCache:
+    return factorialCache[n]
+  f = math.factorial(n)
+  factorialCache[n] = f
+  return f
+
+def combinations(n, r):
+  return int(factorial(n) / (factorial(r) * factorial(n - r)))
+
+def solve(n):
+  count = 0
+  for i in range(1, 101):
+    for j in range(1, i + 1):
+      combos = combinations(i, j)
+      if combos > n:
+        count = count + 1
+  return count
+
+print(solve(1000000))
